af1f4b069a4ee416a459197f27d70f9342bb1d4e
N8387 <n8387@student.jamk.fi>
2020-12-03 16:16:32 +0200

    working voting system

--- part4/blog/index.js
+++ part4/blog/index.js
@@ -1,9 +1,61 @@
-const app = require('./app')
-const http = require('http')
-const config = require('./utils/config')
-const logger = require('./utils/logger')
-const server = http.createServer(app)
+require('dotenv').config()
 
-server.listen(config.PORT, () => {
-  logger.info(`Server running on port ${config.PORT}`)
+const express = require('express')
+const app = express()
+const Blog = require('./models/blog')
+app.use(express.static('build'))
+app.use(express.json())
+
+
+
+app.get('/api/blogs', (request, response) => {
+    Blog.find({}).then(blogs => {
+      response.json(blogs)
+    })
+  })
+
+app.get('/api/blogs/:id', (request, response) => {
+    Blog.findById(request.params.id).then(blog => {
+      response.json(blog)
+    })
 })
+app.post('/api/blogs', (request, response) => {
+  const body = request.body
+  const blog = new Blog({
+    author: body.author,
+    title: body.title,
+    url: body.url,
+    likes: body.likes,
+  })
+
+  blog.save().then(savedNote => {
+    response.json(savedNote)
+  })
+})
+app.delete('/api/blogs/:id', (request, response) => {
+  Blog.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    
+})
+app.put('/api/blogs/:id', (request, response) => {
+  const body = request.body
+
+  const blog = {
+    author: body.author,
+    title: body.title,
+    url: body.url,
+    likes: body.likes,
+  }
+
+  Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+    .then(updatedBlog => {
+      response.json(updatedBlog)
+    })
+})
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
--- part4/blog/models/blog.js
+++ part4/blog/models/blog.js
@@ -1,10 +1,21 @@
 const mongoose = require('mongoose')
 
+const url = process.env.MONGODB_URI
+
+console.log('connecting to', url)
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
 const blogSchema = new mongoose.Schema({
   author: String,
   title: String,
-  blogUrl: String,
-  likes: Number,
+  url: String,
+  likes: Number
 })
 
 blogSchema.set('toJSON', {
