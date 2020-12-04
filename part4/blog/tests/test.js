fb6ac3263f68c4ff12b842b5f3d40f232caf228c
N8387 <n8387@student.jamk.fi>
2020-12-04 10:37:51 +0200

    4.4: apufunktioita ja yksikkötestejä, step2

--- part4/blog/tests/test.js
+++ part4/blog/tests/test.js
@@ -7,3 +7,48 @@ test('dummy returns one', () => {
   expect(result).toBe(1)
 })
 
+describe('total likes', () => {
+    const bloglist = [
+      {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 2,
+        __v: 0
+      },
+      {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 2,
+        __v: 0
+      },
+      {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 4,
+        __v: 0
+      },
+      {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 8,
+        __v: 0
+      }
+    
+    ]
+   
+   const blogsLikes = bloglist.map(blog => blog.likes)
+   const total = blogsLikes.reduce(function(acc, val) { return acc + val; }, 0)
+
+    test('when list has only one blog equals the likes of that', () => {
+      const result = listHelper.totalLikes(total)
+      expect(result).toBe(16)
+    })
+  })
--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -1,15 +1,12 @@
 const dummy = (blogs) => {
    return 1
   }
-  const average = array => {
-    const reducer = (sum, item) => {
-      return sum + item
-    }
-    return array.length === 0
-      ? 0 
-      : array.reduce(reducer, 0) / array.length
+
+  const totalLikes = (blogs) => {
+   console.log(blogs)
+   return [blogs].reduce(function(acc, val) { return acc + val; }, 0)
   }
   module.exports = {
     dummy,
-    average
+    totalLikes
   }
