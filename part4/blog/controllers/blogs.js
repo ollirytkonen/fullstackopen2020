298e614b77e2db969bf656aba2018c152f58439e
N8387 <n8387@student.jamk.fi>
2020-12-09 18:38:45 +0200

    4.19: blogilistan laajennus, step7

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -1,27 +1,40 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog
+    .find({}).populate('user', { username:1, name: 1})
     response.json(blogs.map(blog => blog.toJSON()))
   })
 
-blogsRouter.get('/:id', (request, response, next) => {
-  Blog.findById(request.params.id)
-    .then(blog => {
-      if (blog) {
-        response.json(blog.toJSON())
-      } else {
-        response.status(404).end()
-      }
-    })
-    .catch(error => next(error))
+blogsRouter.get('/:id', async(request, response) => {
+   const blog = await Blog.findById(request.params.id)
+   if (blog) {
+     response.json(blog.toJSON())
+   }else{
+     response.status.apply(404).end()
+   }
 })
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
-blogsRouter.post('/', async (request, response, next) => {
+blogsRouter.post('/', async (request, response) => {
     const body = request.body
-    const user = await User.findById(body.userId)
+    const token = getTokenFrom(request)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+
+    if (!token || !decodedToken.id) {
+      return response.status(401).json({ error: 'token missing or invalid' })
+    }
+
+    const user = await User.findById(decodedToken.id)
 
     const blog = new Blog({
       author: body.author,
@@ -29,41 +42,31 @@ blogsRouter.post('/', async (request, response, next) => {
       url: body.url,
       likes: body.likes,
       user: user._id
+      
     })
-
-    try { 
+    console.log(blog)
       const savedBlog = await blog.save()
       user.blogs = user.blogs.concat(savedBlog._id)
       await user.save()
-
       response.json(savedBlog.toJSON())
-    } catch(exception) {
-      next(exception)
-    }
+    
 })
 
-blogsRouter.delete('/:id', (request, response, next) => {
-    Blog.findByIdAndRemove(request.params.id)
-    .then(result => {
-      response.status(204).end()
-    })
-    .catch(error => next(error))
+blogsRouter.delete('/:id', async (request, response) => {    
+      await Blog.findByIdAndRemove(request.params.id)
+      response.status(204).end()    
 })
 
-blogsRouter.put('/:id', (request, response, next) => {
+blogsRouter.put('/:id', async (request, response) => {
   const body = request.body
   const blog = {
     author: body.author,
     title: body.title,
     url: body.url,
-    likes: body.likes,
+    likes: body.likes
   }
-
-  Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
-    .then(updatedBlog => {
-      response.json(updatedBlog)
-    })
-    .catch(error => next(error))
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })   
+  response.json(updatedBlog.toJSON())
 })
 
 module.exports = blogsRouter
--- part4/blog/controllers/users.js
+++ part4/blog/controllers/users.js
@@ -3,7 +3,9 @@ const usersRouter = require('express').Router()
 const User = require('../models/user')
 
 usersRouter.get('/', async (request, response) => {
-    const users = await User.find({})
+    const users = await User
+    .find({}).populate('blogs', { author: 1, title: 1})
+
     response.json(users.map(u => u.toJSON()))
 })
 usersRouter.post('/', async (request, response) => {
